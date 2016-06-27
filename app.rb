aa529de4445d4707a39232e9e126e4f88035a0e0
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-26 23:52:24 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is _"
+  end
+
+  get '/hometown' do
+    "My hometown is _"
+  end
+
+  get '/favorite-song' do
+    'My favorite song is __'
+  end
 end
